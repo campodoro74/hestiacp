4fe25f10007a0811e22b091bcc280cc4d2e0faa2
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-06-07 12:49:45 +0200

    Update

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -1823,8 +1823,10 @@ if [ "$clamd" = 'yes' ]; then
 		sed -i "s|\[Service\]|[Service]\n$exec_pre1\n$exec_pre2|g" \
 			/lib/systemd/system/clamav-daemon.service
 		systemctl daemon-reload
-		systemctl restart clamav-daemon
 	fi
+	systemctl start clamav-daemon > /dev/null 2>&1
+	sleep 1
+	systemctl status clamav-daemon > /dev/null 2>&1
 	echo -ne "[ * ] Installing ClamAV anti-virus definitions... "
 	/usr/bin/freshclam >> $LOG &
 	BACK_PID=$!
